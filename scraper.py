9ae4c3b62e08486a6f177eec35cec117ba20cf68
Ian <ichen2@lsu.edu>
2021-02-16 14:04:52 -0600

    Set up selenium, created web scraping algorithm

--- scraper.py
+++ scraper.py
@@ -1,5 +1,15 @@
+import os
+import selenium
+from selenium import webdriver
+import time
+import io
 import requests
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
+import csv
+
+driver = webdriver.Chrome('chromedriver.exe')
 
 main_url = 'http://historyview.org/'
 main_page = requests.get(main_url)
@@ -7,15 +17,25 @@ main_page = requests.get(main_url)
 main_soup = BeautifulSoup(main_page.content, 'html.parser')
 
 a_tags = main_soup.find_all(class_='image-wrap')
-page_urls = map(lambda url: url['href'], a_tags)
+page_urls = list(map(lambda url: url['href'], a_tags))
 
-tour_urls = []
+tours = []
 
-#for url in list(page_urls):
-tour_page = requests.get(list(page_urls)[0])
-tour_soup = BeautifulSoup(tour_page.content, 'html.parser')
-print(tour_soup.prettify())
-tour_url = tour_soup.find(id='mp-iframe')['src']
-tour_urls.append(tour_url)
-
-print(tour_urls)
+for url in page_urls:
+    driver.get(url)
+    try:
+        iframe = driver.find_element_by_id('mp-iframe')
+        tour_url = iframe.get_attribute('src')
+        header = driver.find_element_by_class_name('entry-header')
+        tour_title = header.text
+        if (tour_url != None) and (tour_title != ""):
+            tours.append([tour_title, tour_url])
+    except NoSuchElementException:
+        print("mp-iframe not found at " + url)
+for tour in tours:
+    print(tour[0] + ", " + tour[1])
+    
+with open('urls.csv', mode='w', encoding="utf-8") as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=['name', 'url'], lineterminator = '\n')
+    for tour in tours:
+        writer.writerow({'name': tour[0], 'url': tour[1]})
